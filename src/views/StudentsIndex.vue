c350c3fe66d6ff175fbf21e2e2dfd9918f7a5fdc
sun-shin <sunhanshin2@gmail.com>
2020-11-04 12:16:01 -0600

    adds StudentsIndex w/ route and StudentsShow file

---
+++ src/views/StudentsIndex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="students-index">
+    <!-- <div v-for="student in students">
+      <h2>{{}}</h2>
+    </div> -->
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function() {
+    return {
+      students: [],
+    };
+  },
+  created: function() {
+    this.indexStudents;
+  },
+  methods: {
+    indexStudents: function() {
+      axios.get("/api/students").then((response) => {
+        console.log(response.data);
+        this.students = response.data;
+      });
+    },
+  },
+};
+</script>
